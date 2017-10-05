765bc6c03904b0c0163b2795c762b5d099d55217
Adam Short <adam010101@icloud.com>
2017-10-05 13:24:06 +1100

    sdist work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='spookyaction',
-    version='0.1.1',
+    version='0.2.0',
     author='Adam Short',
     author_email='adam010101@icloud.com',
     packages=['spookyaction'],
